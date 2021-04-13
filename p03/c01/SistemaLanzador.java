c1be4cad5455e6a649971398784414761b3487c5
Oscar <xgg1003@alu.ubu.es>
2021-04-13 12:20:20 +0200

    Correccion sistema lanzador

--- p03/c01/SistemaLanzador.java
+++ p03/c01/SistemaLanzador.java
@@ -15,18 +15,11 @@ public class SistemaLanzador {
 			// Creación de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
 			new Thread (entradas).start();
-			new Thread (entradas).start();
-			new Thread (entradas).start();
-			new Thread (entradas).start();
-			new Thread (entradas).start();
+
 			
 			// TODO---------------------------
 			// Creación de hilos de entrada
 			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
-			new Thread (salidas).start();
-			new Thread (salidas).start();
-			new Thread (salidas).start();
-			new Thread (salidas).start();
 			new Thread (salidas).start();
 			
 			
